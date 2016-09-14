6af290700da7a602a53209b66054e4d30ffa5414
mel1bt <mberrocal.tipte@gmail.com>
2016-09-14 13:39:19 -0500

    actualizando mi formulario

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("form-signin-heading").innerHTML="Por favor inicia sesion";
+var input = document.getElementById ("inputEmail");
+input.placeholder = "Correo Electronico";
+var input = document.getElementById ("inputPassword");
+input.placeholder = "Contraseña";
+document.querySelector('span').innerHTML="Recordar datos";
+document.querySelector('button').innerHTML="Iniciar Sesión";
